c270d55d03aca445cc2fae6504cebe66fb74d723
Richard <rsmutch@gmail.com>
2021-04-13 22:03:50 +0100

    Creates meterReadingsUploads  router

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const meterReadingUploads = require('./routes/meterReadingUploads');
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/meter-reading-uploads', meterReadingUploads);
 
 module.exports = app;
---
+++ routes/meterReadingUploads.js
@@ -0,0 +1,5 @@
+const meterReadingUploads = require('express').Router();
+
+meterReadingUploads.route('').post();
+
+module.exports = meterReadingUploads;
