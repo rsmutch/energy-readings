91398ef68ab61e164582b159e4902cc1b56ae4a6
Richard <rsmutch@gmail.com>
2021-04-13 12:55:13 +0100

    Initializes database. node-postgres, knex and nodemon installed

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/energydb',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/production'
+    },
+    useNullAsDefault: true
+  }
+};
