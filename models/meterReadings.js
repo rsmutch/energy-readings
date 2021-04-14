94c1b1a4749e7464d4c882a4cd413e13cf02d77e
Richard <rsmutch@gmail.com>
2021-04-14 14:20:09 +0100

    Added getAllMeterReadings in model

--- models/meterReadings.js
+++ models/meterReadings.js
@@ -3,6 +3,15 @@ const fs = require('fs');
 const csv = require('fast-csv');
 const { validateCsv } = require('../utilities/csvValidation');
 
+exports.getAllMeterReadings = () => {
+  return connection
+    .select('meter_reading_id')
+    .from('meter_readings')
+    .then((readings) => {
+      return readings;
+    });
+};
+
 exports.addMeterReadings = ({ path }) =>
   new Promise((resolve) => {
     let output = {};
